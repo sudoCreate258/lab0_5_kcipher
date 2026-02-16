bd0367ed754574865acd74bf223ba842e5a3683f
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-02-16 05:56:38 -0500

    Add my_functions.py with plaintext and equality check

---
+++ src/my_functions.py
@@ -0,0 +1,20 @@
+# DO NOT UPDATE ANY CODE BELOW THIS LINE
+import random as rand
+
+def get_plaintext():
+    def fn():
+        seed = rand.randint(-25,25)
+        ascii_A = 65
+        alst = [chr(x) for x in range(ascii_A, ascii_A+26)]
+        return alst[seed]
+    return f"{fn()}{fn()}{fn()}"
+    
+def check_equals(ptext,ctext,flag=True):
+    if flag:
+        exp = ''.join([chr(ord(p)-3) for p in ptext])
+        dxp = ''.join([chr(ord(p)+3) for p in exp])
+    else:
+        exp = ''.join([chr(ord(p)*3) for p in ptext])
+        dxp = ''.join([chr(int(ord(p)/3)) for p in exp])
+    assert ptext == dxp
+    return exp == ctext
