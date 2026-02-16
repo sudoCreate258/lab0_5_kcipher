7506ad95997ce28097bc70b45a1e04a1d300513f
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2026-02-16 05:55:38 -0500

    Initialize main.py with function imports and TODOs
    Add initial structure and TODOs for main.py

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from my_functions import get_plaintext, check_equals
+# DO NOT UPDATE CODE ABOVE THIS LINE
+
+#TODO: (Input)   Assign data to the variables.
+#TODO: (Process) Manipulate the string.
+#TODO: (Output)  Display the asserted cipher text.
+
+'''
+# Ready for the extra credit?
+
+assert check_equals(exp,act,False), "this should fail"
+'''
